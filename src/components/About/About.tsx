151d9b4cd7367536bc85f4274bebcae2f4838513
vijaymca119 <99117224+vijaymca119@users.noreply.github.com>
2025-01-23 07:56:08 +0000

    fix

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../assets/about.png";
+import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
 
 const About: React.FC = () => {
